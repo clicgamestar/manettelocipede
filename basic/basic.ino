0ec086c2a37bbbd6057292b3f3555cc44ab7ba33
Stache <hugo.hueber@arcanite.ch>
2020-10-15 03:57:11 +0200

    Add button to basic example

--- basic/basic.ino
+++ basic/basic.ino
@@ -2,17 +2,25 @@
 
 #define SPEED_THRESHOLD 15
 #define PLAYER_1_UP 'z'
+#define PLAYER_1_LEFT 'q'
+#define PLAYER_1_RIGHT 'd'
 #define PLAYER_2_UP KEY_UP_ARROW
+#define PLAYER_2_LEFT KEY_LEFT_ARROW
+#define PLAYER_2_RIGHT KEY_RIGHT_ARROW
 
 /* Vélal A */
 int wheelInputA = 2;
 int speedCounterA = 0;
 int old_wheelStateA;
+int leftInputA = 4;
+int rightInputA = 5;
 
 /* Vélal B */
 int wheelInputB = 6;
 int speedCounterB = 0;
 int old_wheelStateB;
+int leftInputB = 8;
+int rightInputB = 9;
 
 /* Time */
 int time = 0;
@@ -33,7 +41,7 @@ void gottaGoFast(int wheelInput, int* oldState, int* counter, int threshold, cha
   if(seconds() != time) {
     time = seconds();
     *counter = 0;
-    Keyboard.releaseAll();
+    Keyboard.release(command);
   }
 
   Serial.print("gottaGoFast? ");
@@ -50,6 +58,8 @@ void setup() {
   /* Vélal A */
   pinMode(wheelInputA, INPUT);
   old_wheelStateA = digitalRead(wheelInputA);
+  pinMode(leftInputA, INPUT);
+  pinMode(rightInputA, INPUT);
   
   /* Vélal B */
   pinMode(wheelInputB, INPUT);
@@ -61,7 +71,29 @@ void setup() {
 }
 
 void loop() {
+  // Vélal A
   gottaGoFast(wheelInputA, &old_wheelStateA, &speedCounterA, SPEED_THRESHOLD, PLAYER_1_UP);
+
+  if(digitalRead(leftInputA) == HIGH && digitalRead(rightInputA) == LOW) {
+    Keyboard.press(PLAYER_1_LEFT);
+  } else if(digitalRead(leftInputA) == LOW && digitalRead(rightInputA) == HIGH) {
+    Keyboard.press(PLAYER_1_RIGHT);
+  } else {
+    Keyboard.release(PLAYER_1_LEFT);
+    Keyboard.release(PLAYER_1_RIGHT);
+  }
+
+  // Vélal B
   gottaGoFast(wheelInputB, &old_wheelStateB, &speedCounterB, SPEED_THRESHOLD, PLAYER_2_UP);
+
+  if(digitalRead(leftInputB) == HIGH && digitalRead(rightInputB) == LOW) {
+    Keyboard.press(PLAYER_2_LEFT);
+  } else if(digitalRead(leftInputB) == LOW && digitalRead(rightInputB) == HIGH) {
+    Keyboard.press(PLAYER_2_RIGHT);
+  } else {
+    Keyboard.release(PLAYER_2_LEFT);
+    Keyboard.release(PLAYER_2_RIGHT);
+  }
+  
   delay(1);
 }
